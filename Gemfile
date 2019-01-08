4f3dce8cb55a6c8af47b22d1b6eb1adfd01fc9cf
matt-mcalister <matt.mcalister93@gmail.com>
2019-01-08 10:28:30 -0500

    listing now assigns its source on create

--- Gemfile
+++ Gemfile
@@ -35,6 +35,11 @@ gem 'jbuilder', '~> 2.5'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+# scraping
+gem 'httparty'
+gem 'nokogiri'
+gem 'json'
+
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
 
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,41 +1,40 @@
 class Listing < ApplicationRecord
 
   attr_writer :response, :parsed_page
-  after_save
+  after_create :assign_source
   @@options = {
     headers: {
       accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
       "accept-encoding": "gzip, deflate, br",
       "accept-language": "en-US,en;q=0.9,en-GB;q=0.8",
       "cache-control": "no-cache",
-      cookie: ,
+      cookie: ENV["COOKIE"],
       pragma: "no-cache",
       "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"
     }
   }
 
-def initialize(url)
-  @url = url
-  @source = url.split(".com").first.split("://").last.split("www.").first
-end
-
   def assign_source
-
+    self.source = url.split(".com").first.split("://").last.split("www.").first
+    self.save
   end
 
-def fetch_data
-  self.response = HTTParty.get(self.url, @@options)
-end
+  def fetch_data
+    self.response = HTTParty.get(self.url, @@options)
+  end
 
-def response
-  @response || self.fetch_data
-end
+  def response
+    @response || self.fetch_data
+  end
 
-def json
-  JSON.parse(self.response.body)
-end
+  def parsed_page
+    @parsed_page || self.parsed_page = Nokogiri::HTML(self.response)
+  end
 
-def parsed_page
-  @parsed_page || self.parsed_page = Nokogiri::HTML(self.response)
-end
+  def assign_values
+    case self.source
+    when "streeteasy"
+      puts "doin' that streeteasy thing"
+    end
+  end
 end
