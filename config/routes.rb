b5b8b170fb606387bd0338ae3f55103ac9886946
matt-mcalister <matt.mcalister93@gmail.com>
2019-01-10 11:51:25 -0500

    added /listings/search route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :listings, only: [ :create, :destroy]
-  post "/listings", to: 'listings#search'
+  post "/listings/search", to: 'listings#search'
 
   root to: 'listings#index'
 end
