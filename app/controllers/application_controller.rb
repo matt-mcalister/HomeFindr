c994553f7f2266cefebaacde038aa44bce8edc49
matt-mcalister <matt.mcalister93@gmail.com>
2019-01-10 15:42:41 -0500

    generated devise views and setup basic landing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_dog!, except: [:signup, :login, :landing]
+  before_action :authenticate_user!, except: [:sign_in, :sign_out, :password, :confirmation, :registration, :edit]
   before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
 
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
   def landing
-    
+    if current_user
+      @woofs = Woof.all
+      render :home
+    else
+      render :landing
+    end
   end
 end
