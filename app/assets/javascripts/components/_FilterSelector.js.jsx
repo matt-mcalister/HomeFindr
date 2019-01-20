43a4a8a59bcc7da196ac69e25561e4bcff88b06e
matt-mcalister <matt.mcalister93@gmail.com>
2019-01-20 10:39:19 -0500

    created basic filter selection component

---
+++ app/assets/javascripts/components/_FilterSelector.js.jsx
@@ -0,0 +1,30 @@
+const labels = {
+  all: "All",
+  no_fee: "No Fee",
+  dishwasher: "Dishwasher",
+  guarantors_accepted: "Guarantors Accepted",
+  outdoor_space: "Outdoor Space",
+  laundry_in_unit: "Laundry In Unit",
+  laundry_in_building: "Laundry In Building",
+  doorman: "Doorman",
+  package_handling: "Package Handling",
+  elevator: "Elevator",
+  gym: "Gym",
+}
+
+
+const FilterSelector = ({ filterBy, setFilter }) => {
+  let filterAttrs = Object.keys(filterBy)
+  return (
+    <div id="filter-checks">
+    { filterAttrs.map(attr => {
+      return (
+        <React.Fragment key={attr}>
+          <label htmlFor={attr}>{labels[attr]}</label>
+          <input type="checkbox" name={attr} checked={filterBy[attr]} onChange={e => setFilter(attr)}/>
+        </React.Fragment>
+      )
+    }) }
+    </div>
+  )
+}
--- app/assets/javascripts/components/_HomePage.js.jsx
+++ app/assets/javascripts/components/_HomePage.js.jsx
@@ -5,10 +5,23 @@ class HomePage extends React.Component {
     this.state = {
       selectedHuntId: props.selectedHuntId,
       hunts: props.hunts,
-      filterBy: {},
+      filterBy: {
+        all: true,
+        no_fee: false,
+        dishwasher: false,
+        guarantors_accepted: false,
+        outdoor_space: false,
+        laundry_in_unit: false,
+        laundry_in_building: false,
+        doorman: false,
+        package_handling: false,
+        elevator: false,
+        gym: false,
+      },
       sortBy: "all"
     }
     this.selectHunt = this.selectHunt.bind(this)
+    this.setFilter = this.setFilter.bind(this)
   }
 
   selectHunt(e){
@@ -27,11 +40,16 @@ class HomePage extends React.Component {
     return listings
   }
 
+  setFilter(attr){
+    console.log(attr)
+  }
+
   render() {
     console.log(this.state);
     return (
       <div className="page-content home">
         <HuntSelector selectedHuntId={this.state.selectedHuntId} hunts={this.state.hunts} selectHunt={this.selectHunt}/>
+        <FilterSelector filterBy={this.state.filterBy} setFilter={this.setFilter}/>
         <ListingsList listings={this.sorted(this.filtered())}/>
       </div>
     )
