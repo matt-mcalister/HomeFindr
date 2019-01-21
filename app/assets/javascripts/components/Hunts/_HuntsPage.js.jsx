9aa9c0dcd9834b8398ba7485fbfd13ffed485cb7
matt-mcalister <matt.mcalister93@gmail.com>
2019-01-21 13:36:22 -0500

    setup new hunt form

--- app/assets/javascripts/components/Hunts/_HuntsPage.js.jsx
+++ app/assets/javascripts/components/Hunts/_HuntsPage.js.jsx
@@ -1,10 +1,38 @@
-const HuntsPage = ({ hunts }) => {
-  let huntIds = Object.keys(hunts)
-  return (
-    <div className="page-content hunts">
-      <div className="box-list">
-      { huntIds.map(id => <EditHunt key={id} hunt={hunts[id]}/>)}
+class HuntsPage extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      hunts: props.hunts,
+      newHunt: ""
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(e){
+    this.setState({
+      newHunt: e.target.value
+    })
+  }
+
+  handleSubmit(e){
+    e.preventDefault()
+    console.log(this.state.newHunt);
+  }
+
+  render(){
+    let huntIds = Object.keys(this.state.hunts)
+    return (
+      <div className="page-content hunts">
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="newHunt">Create Hunt:</label>
+          <input type="text" name="newHunt" value={this.state.newHunt} onChange={this.handleChange} />
+          <input type="submit" />
+        </form>
+        <div className="box-list">
+          { huntIds.map(id => <EditHunt key={id} hunt={this.state.hunts[id]}/>)}
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
