665f418af3b717fa885f74dc86bc9e643cbf347a
matt-mcalister <matt.mcalister93@gmail.com>
2019-01-21 10:09:22 -0500

    added favicon

---
+++ app/assets/javascripts/components/Hunts/_HuntsPage.js.jsx
@@ -0,0 +1,11 @@
+const HuntsPage = ({ hunts }) => {
+  return (
+    <div className="page-content hunts">
+      // make new hunt
+      // manage each hunt
+        // add/delete users
+        // change name
+        // delete hunt
+    </div>
+  )
+}
