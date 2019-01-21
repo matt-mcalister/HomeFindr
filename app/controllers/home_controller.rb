c189db89dd27acdf543907d9e355f1f4bba68118
matt-mcalister <matt.mcalister93@gmail.com>
2019-01-21 14:25:43 -0500

    added handling for homepage if user has no hunts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,12 @@ class HomeController < ApplicationController
   def landing
     if current_user
       @hunts = current_user.hunts_with_listings
-      @selected_hunt_id = current_user.hunts.last.id
-      render :home
+      if !current_user.hunts.last
+        redirect_to hunts_path
+      else
+        @selected_hunt_id = current_user.hunts.last.id
+        render :home
+      end
     else
       render :landing
     end
